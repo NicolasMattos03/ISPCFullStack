bf9a757c2d050463d884c020b2c9154223a61e82
DevAleMoCa <morenocabreroalejandro@gmail.com>
2022-10-21 12:01:22 -0300

    reorden y script whatsapp

---
+++ MVC/Vista/js/script2.js
@@ -0,0 +1,30 @@
+popupWhatsApp = () => {
+  
+  let btnClosePopup = document.querySelector('.closePopup');
+  let btnOpenPopup = document.querySelector('.whatsapp-button');
+  let popup = document.querySelector('.popup-whatsapp');
+  let sendBtn = document.getElementById('send-btn');
+
+  btnClosePopup.addEventListener("click",  () => {
+    popup.classList.toggle('is-active-whatsapp-popup')
+  })
+  
+  btnOpenPopup.addEventListener("click",  () => {
+    popup.classList.toggle('is-active-whatsapp-popup')
+     popup.style.animation = "fadeIn .6s 0.0s both";
+  })
+  
+  sendBtn.addEventListener("click", () => {
+  let msg = document.getElementById('whats-in').value;
+  let relmsg = msg.replace(/ /g,"%20");
+     
+   window.open('https://wa.me/521557153381?text='+relmsg, '_blank'); 
+  
+  });
+
+  setTimeout(() => {
+    popup.classList.toggle('is-active-whatsapp-popup');
+  }, 3000);
+}
+
+popupWhatsApp();
