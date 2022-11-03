076afdd29284e9318bdc386f39dd726c46ab1a1b
nahuonaindia <codnahuu@gmail.com>
2022-11-03 00:02:30 -0300

    Edit

--- Backend/Controlador/busqueda.py
+++ Backend/Controlador/busqueda.py
@@ -5,12 +5,18 @@
     # MayorPrecioPesos
     # MasNuevos
     # MasAngtiguos
+
 # TipoDeInumebles()
+
 # Estado()
     # EnVenta
     # EnAlquiler
     # EnAlquilerTemporal
+
 # ZonasInmuebles()
+
 # CantidadDormitorios()
+
 # rangoPrecios()
+
 # km2()
