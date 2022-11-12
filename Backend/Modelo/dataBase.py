0e67f6e72c203f2295068658359cd4c19f79486a
nahuonaindia <codnahuu@gmail.com>
2022-11-12 19:55:45 -0300

    Actualizando Rama 12-11-22

--- Backend/Modelo/dataBase.py
+++ Backend/Modelo/dataBase.py
@@ -53,5 +53,15 @@
     # nombre: varchar
     # apellido: varchar
     # e-mail: varchar not null
+    
+# CONTRATOS
+    # id_contrato: int - PK
+    # id_inmueble: int - FK
+    # id_venta: int - FK
+    # id_alquiler: int - FK 
+    # DNI_cliente: int - FK
+    # fecha_inicio: dateTime
+    # fecha_fin: dateTime
+    # fecha_revision: dateTime
 
 
